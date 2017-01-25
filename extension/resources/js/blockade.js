646592080696278ef50feb4e5e1186e39b2e976e
Brandon Dixon <brandon@9bplus.com>
2017-01-25 01:04:36 -0800

    Continued work on #8. - I lied on the testing, shame on me. Fixed a few bugs and _now_ it works.

--- extension/resources/js/blockade.js
+++ extension/resources/js/blockade.js
@@ -4,9 +4,10 @@ var BlockadeIO = {
     sources: [],
 
     init: function() {
-        if (localStorage.getItem("indicators") !== null) {
+        if (localStorage.getItem("cfg_indicators") !== null) {
             console.log("Loading signatures from storage");
-            LZMA.decompress(localStorage.cfg_indicators, function(decoded, error) {
+            var data = JSON.parse(localStorage.cfg_indicators);
+            LZMA.decompress(data, function(decoded, error) {
                 BlockadeIO.indicators = JSON.parse(decoded);
                 BlockadeIO.active = true;
             });
@@ -39,7 +40,7 @@ var BlockadeIO = {
         var store = JSON.stringify(indicators);
         LZMA.compress(store, 1, function(encoded, error) {
             if (localStorage.cfg_debug === 'true') { console.log("Compressing"); }
-            localStorage.cfg_indicators = encoded;
+            localStorage.cfg_indicators = JSON.stringify(encoded);
         });
         BlockadeIO.indicatorCount = Object.keys(indicators).length;
         BlockadeIO.indicators = indicators;
